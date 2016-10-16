62950037bcfdf1f4b7a7fb17b14ba4d1f1ba9bca
Nicolas <nicolastorero@gmail.com>
2016-10-16 23:17:34 +0200

    WIP

--- src/main/java/fr/afcepf/atod/ws/currency/exception/CurrenciesWSError.java
+++ src/main/java/fr/afcepf/atod/ws/currency/exception/CurrenciesWSError.java
@@ -12,5 +12,17 @@ public enum CurrenciesWSError {
     /**
      * Objet demandé non présent en base.
      */
-    RECHERCHE_NON_PRESENTE_EN_BASE
+    RECHERCHE_NON_PRESENTE_EN_BASE,
+    /**
+     * Ajout dans la base impossible.
+     */
+    IMPOSSIBLE_AJOUT_DANS_BASE,
+    /**
+     * Suppression dans la base impossible.
+     */
+    IMPOSSIBLE_SUPPRESSION_DANS_BASE,
+    /**
+     * L'update ne peut être faite.
+     */
+    UPDATE_NON_EFFECTUE_EN_BASE
 }
